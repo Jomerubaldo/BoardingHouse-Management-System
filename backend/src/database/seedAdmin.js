384096261be3e83f202daeea258569b10264ce43
Your Name <jomerubaldo@gmail.com>
2026-07-26 04:51:37 +0800

    seedAdmin hashing password

---
+++ backend/src/database/seedAdmin.js
@@ -0,0 +1,27 @@
+import db from '../config/db.js';
+import bcrypt from 'bcryptjs';
+
+const saltRounds = 10;
+
+const username = 'admin1';
+const password = 'admin1';
+
+const seedAdmin = async () => {
+  try {
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+
+    const sql = `INSERT INTO admin (username, password) VALUES (?, ?) `;
+
+    db.query(sql, [username, hashPassword], (err, result) => {
+      if (err) {
+        console.error('Error seeding admin', err);
+        return;
+      }
+      console.log('Admin successfully seeded', result);
+    });
+  } catch (err) {
+    console.error('Error hashing password:', err);
+  }
+};
+
+seedAdmin();
